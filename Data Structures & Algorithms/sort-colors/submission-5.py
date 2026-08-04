b9088ff95683dafcfc93e2cc03f467cf9140900a
Brandon Oliver <104800987+btso8@users.noreply.github.com>
2026-08-04 23:28:06 +0100

    Add: sort-colors - submission-5

---
+++ Data Structures & Algorithms/sort-colors/submission-5.py
@@ -0,0 +1,21 @@
+class Solution:
+    def sortColors(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        l = 0
+        r = len(nums) - 1
+        i = 0
+        while i <= r:
+            if nums[i] == 0:
+                temp = nums[i]
+                nums[i] = nums[l]
+                nums[l] = temp
+                l += 1
+            elif nums[i] == 2:
+                temp = nums[i]
+                nums[i] = nums[r]
+                nums[r] = temp
+                r -= 1
+                i -= 1
+            i += 1
