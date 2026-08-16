42664d365acf81b2f8bff1f276c24e0e8f19b351
Brandon Oliver <104800987+btso8@users.noreply.github.com>
2026-08-16 23:01:39 +0100

    Add: majority-element-ii - submission-11

---
+++ Data Structures & Algorithms/majority-element-ii/submission-11.py
@@ -0,0 +1,17 @@
+class Solution:
+    def majorityElement(self, nums: List[int]) -> List[int]:
+        count_hashmap = defaultdict(int)
+        for num in nums:
+            count_hashmap[num] += 1
+            if len(count_hashmap) <= 2:
+                continue
+            new_count_hashmap = defaultdict(int)
+            for num, count in count_hashmap.items():
+                if count > 1:
+                    new_count_hashmap[num] = count - 1
+            count_hashmap = new_count_hashmap    
+        result = []
+        for num in count_hashmap:
+            if nums.count(num) > len(nums) // 3:
+                result.append(num)
+        return result
